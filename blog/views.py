e1ab5d2f6da78a1b2aaa6f9569814f2b52bc7105
Tom Challenger <too.mail.tom@gmail.com>
2017-10-03 16:47:57 +0300

    Added view and templete for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone # Подключение библиотеки работы со временем
+
 
 # Create your views here.
 
@@ -8,3 +9,7 @@ from django.utils import timezone # Подключение библиотеки 
 def post_list(request): #reuest - это все что мы получили от пользателя через Инет
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
