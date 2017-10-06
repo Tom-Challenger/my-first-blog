e0f88e7639ac97e0b7a3b83f3299feb06476eb74
Tom Challenger <too.mail.tom@gmail.com>
2017-10-06 12:35:54 +0300

    Homework: create comment model - Complete

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 #from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)  # Регистрируем нашу модель - что бы она стала доступна на странице администрирования
+admin.site.register(Comment)
--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,16 @@
 from django import forms 
-from .models import Post #импортировать модель Post
+from .models import Post, Comment
 #импортировать модуль Django.forms
+#импортировать модель Post
+#импортировать модель Comment
+
 class PostForm(forms.ModelForm):
 	class Meta:  
 		#какая модель будет использоваться для создания формы (model=Post)
 		model = Post
 		fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ('author', 'text',)
